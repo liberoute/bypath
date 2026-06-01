c7c0c2b46c420c744b097c24e9904ea6a97f09b4
Yasin Hamidi <yaasinhamidi@gmail.com>
2026-06-01 05:36:06 +0330

    v2.5.2

--- internal/build/branding.go
+++ internal/build/branding.go
@@ -12,7 +12,7 @@ import (
 // These variables are set at build time via ldflags.
 var (
 	Name      = "Bypath"
-	Version   = "2.5.1"
+	Version   = "2.5.2"
 	Org       = "Liberoute"
 	Commit    = "unknown"
 	BuildDate = "unknown"
